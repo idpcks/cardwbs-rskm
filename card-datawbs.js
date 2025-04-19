ebe778d86d756b67bbc7b396f9a78f18e608ca1a
idpcks <136402646+idpcks@users.noreply.github.com>
2025-04-19 10:58:54 +0700

    Create card-datawbs.js

---
+++ card-datawbs.js
@@ -0,0 +1,37 @@
+const cardsData = [
+  {
+    title: "Penetapan WBS",
+    description: "SK Direktur Terkait Penetapan WBS PT KRAKATAU MEDIKA.",
+    pdfId: "1AgI2WH9e6HLE8dXvmIeBUxSOTlPp3DVp"
+  },
+  {
+    title: "Board Manual PT KM",
+    description: "",
+    pdfId: "16PWt2K6dxrmZVNALAVvZQfgJv-9_XfH1"
+  },
+  {
+    title: "PENETAPAN PEDOMAN GCG PT KM",
+    description: "Penetapan Pedoman Good Corporate Governance PT Krakatau Medika",
+    pdfId: "1yDYE_GubIg3pUgzOuwfAQNlBX9xNBb8h"
+  },
+  {
+    title: "PENETAPAN PEDOMAN ETIKA BISNIS DAN ETIKA KERJA (COC) PT KM",
+    description: "Penetapan Pedoman Etika Bisnis dan Etika Kerja (Code of Conduct) PT Krakatau Medika",
+    pdfId: "1K0xeSauS8TKsdi0poUAa0JIovBQ_INKN"
+  },
+  {
+    title: "CAPAIAN KOMISARIS PT KM TAHUN 2024",
+    description: "",
+    pdfId: "1TOTxTiG33iGPn94UZ10D6-gCN2kI2QOk"
+  },
+  {
+    title: "KPI TAHUN 2024 (FINAL)",
+    description: "-",
+    pdfId: "1Q679XHRYel0X2TG5PYM2aLwqJvPQXB41"
+  },
+  {
+    title: "KPI TARGET 2024-KM_241112",
+    description: "",
+    pdfId: "1mHyEhY6BYzCVxsj_0GPqk_vxQ6vVXxQv"
+  }
+];
